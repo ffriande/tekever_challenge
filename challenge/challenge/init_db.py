03bc65bc5d3dc61230e02e6cc21a51fafada4c8d
Francisco Friande <friande@MacBook-Air-de-Francisco.local>
2022-09-10 02:52:35 +0100

    create endpoints

--- challenge/challenge/init_db.py
+++ challenge/challenge/init_db.py
@@ -1,4 +1,4 @@
-from .models import Customer
+from .models import Customer, Account, Transaction
 from .extensions import db
 
 
@@ -11,7 +11,19 @@ def init_db(app):
         customer2 = Customer(name='Pedro', surname='Soares')
         customer3 = Customer(name='Francisca', surname='Silva')
         customer4 = Customer(name='Joana', surname='Costa')
+    
+        transaction1 = Transaction(value=0)
+        transaction2 = Transaction(value=0)
+        transaction3 = Transaction(value=10)
+        transaction4 = Transaction(value=10)
 
+        account1 = Account(balance=0, transactions=[transaction1], customer_id=1)
+        account2 = Account(balance=0, transactions=[transaction2], customer_id=2)
+        account3 = Account(balance=10, transactions=[transaction3], customer_id=3)
+        account4 = Account(balance=10, transactions=[transaction4], customer_id=4)
+    
         db.session.add_all([ customer1,  customer2,  customer3, customer4])
+        db.session.add_all([ transaction1,  transaction2,  transaction3, transaction4])
+        db.session.add_all([ account1,  account2,  account3, account4])
 
         db.session.commit()
--- challenge/challenge/views.py
+++ challenge/challenge/views.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, request
-
+from .extensions import db
+from .models import Customer, Account, Transaction
+import json 
 main = Blueprint('main', __name__)
 
     
@@ -13,7 +15,54 @@ def create_account():
     content_type = request.headers.get('Content-Type')
     if (content_type == 'application/json'):
         json = request.json
+        print(json)
+
+        if not ('customerID' in json and 'initialCredit' in json):
+            return 'Payload fields incorrect'
+
+        balance = json['initialCredit']
+        transactions = []
+        
+        if balance != 0:
+            t = Transaction(value=balance)
+            transactions.append(t)
+            db.session.add(t)
+        
+        new_account = Account(balance=balance, transactions=transactions, customer_id=json['customerID'])
+        db.session.add(new_account)
+        
+        db.session.commit()
+        
         return json
     else:
         return 'Content-Type not supported!'
     return 
+
+@main.route('/user_info', methods=['GET'])
+def user_info():
+    
+    args = request.args
+    
+    if 'customerID' not in args:
+        return 'Parameters fields are incorrect!'    
+            
+    customer = Customer.query.get(args['customerID'])
+    
+    balance = 0
+    transactions = []
+    
+    for acc in customer.accounts:
+        balance += acc.balance
+        for trans in acc.transactions:
+            transactions.append(str(trans))
+    
+    return json.dumps({'name': customer.name, 'surname': customer.surname, 'balance': balance, 'accounts_transactions':transactions})
+    return 
+
+
+@main.route('/dump_db', methods=['GET'])
+def dump_db():
+    custs = Customer.query.all()
+    trans = Transaction.query.all()
+    accs = Account.query.all()
+    return f'{custs}\n{trans}\n{accs}'
